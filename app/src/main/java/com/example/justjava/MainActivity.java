fb84bb235e95334be652e51585d84493bce0ea68
manglesh-0605 <manglesh0605@gmail.com>
2021-02-08 18:03:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    private void display(int number){
+        TextView quantityTextView=(TextView) findViewById(R.id.quantity);
+
+        quantityTextView.setText(""+number);
+    }
+}
